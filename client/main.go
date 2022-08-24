435f8171a9c691dad66140d1b7ab601f7931e771
Claudio Emmanuel <claudio.souza@linx.com.br>
2022-08-24 15:46:30 -0300

    init

---
+++ client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewHelloServiceClient(connection)
+
+	Hello(err, client)
+}
+
+func Hello(err error, client pb.HelloServiceClient) {
+	if err != nil {
+		log.Fatalf("failed to greet: %v", err)
+	}
+
+	response, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "World"})
+	if err != nil {
+		log.Fatalf("failed to greet: %v", err)
+	}
+
+	log.Printf("Greeting: %s", response.Message)
+}
---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterHelloServiceServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello " + in.Name}, nil
+}
